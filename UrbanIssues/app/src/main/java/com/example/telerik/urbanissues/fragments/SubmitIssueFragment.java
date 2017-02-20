c9c07d5a1bcae575dff753124d1aa15772101da3
g-yonchev <georgi.d.yonchev@gmail.com>
2017-02-20 21:26:50 +0200

    Submit issue create sample layout

--- UrbanIssues/app/src/main/java/com/example/telerik/urbanissues/fragments/SubmitIssueFragment.java
+++ UrbanIssues/app/src/main/java/com/example/telerik/urbanissues/fragments/SubmitIssueFragment.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.telerik.urbanissues.R;
+import com.example.telerik.urbanissues.models.IssueType;
 
 public class SubmitIssueFragment extends Fragment {
 
@@ -16,6 +19,23 @@ public class SubmitIssueFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_submit_issue, container, false);
 
+
+        Spinner dropdown = (Spinner) rootView.findViewById(R.id.spinner_create_issue_categories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_spinner_dropdown_item, getIssueTypes());
+        dropdown.setAdapter(adapter);
+
         return rootView;
+    }
+
+    private String[] getIssueTypes() {
+        String[] items = new String[IssueType.values().length];
+
+        int i = 0;
+        for (IssueType issueType : IssueType.values()) {
+            items[i] = issueType.toString();
+            i++;
+        }
+
+        return items;
     }
 }
